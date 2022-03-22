6a6a461c5eff49915406aaf18d675f89fe6a367e
duehrsen <duehrsen@gmail.com>
2022-03-21 23:33:34 -0700

    Initial Commit

---
+++ TimeForABreakSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeForABreakSwiftUI
+//
+//  Created by Chris Duehrsen on 2022-03-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeForABreakSwiftUI/TimeForABreakSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeForABreakSwiftUIApp.swift
+//  TimeForABreakSwiftUI
+//
+//  Created by Chris Duehrsen on 2022-03-21.
+//
+
+import SwiftUI
+
+@main
+struct TimeForABreakSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
