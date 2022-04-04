d58e929be47d43033777df9fd2966c4142e1a29d
duehrsen <duehrsen@gmail.com>
2022-04-03 21:37:40 -0700

    Made timer a button Commented out play / pause button

--- TimeForABreakSwiftUI/TimerCountView.swift
+++ TimeForABreakSwiftUI/TimerCountView.swift
@@ -33,51 +33,59 @@ struct TimerCountView: View {
     var body: some View {
         
         let diameter : CGFloat = 200
-        let outofSize : CGFloat = 20
-        let timerTextSize : CGFloat = 40
+        //let outofSize : CGFloat = 10
+        let timerTextSize : CGFloat = 50
+        let playIconSize : CGFloat = 50
         let lineWidth : CGFloat = 30
         
         VStack(spacing: 60) {
             Spacer()
             Text(tm.isWorkTime ? "Workin' time left" : "Chillin' Time Left")
                 .font(.largeTitle)
-            ZStack {
-                Circle()
-                    .trim(from: 0, to: 1)
-                    .stroke(Color.black.opacity(0.09),style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
-                    .frame(width: diameter, height: diameter)
-                Circle()
-                    .trim(from: 0, to: tm.to)
-                    .stroke(Color.blue,style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
-                    .frame(width: diameter, height: diameter)
-                    .rotationEffect(.init(degrees: -90))
-                VStack {
-                    Text("\(convertSecondsToTime(timeinSeconds:tm.currentTimeRemaining))")
-                        .font(.system(size: timerTextSize))
-                        .fontWeight(.bold)
-                    Text("out of \(convertSecondsToTime(timeinSeconds: tm.isWorkTime ? tm.workTimeTotalSeconds : tm.breakTimeTotalSeconds))")
-                        .font(.system(size: outofSize))
+            Button {
+                tm.started.toggle()
+            } label: {
+                ZStack {
+                    Circle()
+                        .trim(from: 0, to: 1)
+                        .stroke(Color.black.opacity(0.09),style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                        .frame(width: diameter, height: diameter)
+                    Circle()
+                        .trim(from: 0, to: tm.to)
+                        .stroke(Color.blue,style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                        .frame(width: diameter, height: diameter)
+                        .rotationEffect(.init(degrees: -90))
+                    VStack {
+                        Text("\(convertSecondsToTime(timeinSeconds:tm.currentTimeRemaining))")
+                            .font(.system(size: timerTextSize))
+                            .fontWeight(.bold)
+                        Label("", systemImage: tm.started ? "play.fill" : "pause.fill")
+                            .foregroundColor(.blue)
+                            .font(.system(size: playIconSize))
+                        //Text("out of \(convertSecondsToTime(timeinSeconds: tm.isWorkTime ? tm.workTimeTotalSeconds : tm.breakTimeTotalSeconds))").font(.system(size: outofSize))
+                    }
+                    
                 }
-                
             }
+
             
             HStack (spacing: 20) {
-                Button(action: {
-                    tm.started.toggle()
-                    
-                }) {
-                    HStack(spacing: 15){
-                        Image(systemName: tm.started ? "pause.fill" : "play.fill")
-                            .foregroundColor(.white)
-                        Text(tm.started ? "Pause" : "Play")
-                            .foregroundColor(.white)
-                    }
-                    .padding(.vertical)
-                    .frame(width: (UIScreen.main.bounds.width / 3) - 20)
-                    .background(Color.blue)
-                    .clipShape(Capsule())
-                    .shadow(radius: 5)
-                }
+//                Button(action: {
+//                    tm.started.toggle()
+//
+//                }) {
+//                    HStack(spacing: 15){
+//                        Image(systemName: tm.started ? "pause.fill" : "play.fill")
+//                            .foregroundColor(.white)
+//                        Text(tm.started ? "Pause" : "Play")
+//                            .foregroundColor(.white)
+//                    }
+//                    .padding(.vertical)
+//                    .frame(width: (UIScreen.main.bounds.width / 3) - 20)
+//                    .background(Color.blue)
+//                    .clipShape(Capsule())
+//                    .shadow(radius: 5)
+//                }
                 
                 Button(action: {
                     tm.currentTimeRemaining = tm.isWorkTime ? tm.workTimeTotalSeconds : tm.breakTimeTotalSeconds
@@ -87,16 +95,14 @@ struct TimerCountView: View {
                 }) {
                     HStack(spacing: 15){
                         Image(systemName: "arrow.clockwise")
-                            .foregroundColor(.blue)
+                            .foregroundColor(.white)
                         Text("Restart")
-                            .foregroundColor(.blue)
+                            .foregroundColor(.white)
                     }
                     .padding(.vertical)
-                    .frame(width: (UIScreen.main.bounds.width / 3) - 20)
-                    .background(
-                        Capsule()
-                            .stroke(Color.blue, lineWidth: 2)
-                    )
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 55)
+                    .background(Color.blue)
+                    .clipShape(Capsule())
                     .shadow(radius: 5)
                     
                 }
@@ -106,16 +112,14 @@ struct TimerCountView: View {
                 }) {
                     HStack(spacing: 15){
                         Image(systemName: "rectangle.portrait.and.arrow.right")
-                            .foregroundColor(.green)
+                            .foregroundColor(.white)
                         Text("To " + (tm.isWorkTime ? "Break" : "Work"))
-                            .foregroundColor(.green)
+                            .foregroundColor(.white)
                     }
                     .padding(.vertical)
-                    .frame(width: (UIScreen.main.bounds.width / 3) - 20)
-                    .background(
-                        Capsule()
-                            .stroke(Color.green, lineWidth: 2)
-                    )
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 55)
+                    .background(Color.green)
+                    .clipShape(Capsule())
                     .shadow(radius: 5)
                     
                 }
